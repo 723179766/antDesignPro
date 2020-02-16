8c3f849fd990ef5dee9691b8311af008811936b0
houchaoyu <723179766@qq.com>
2020-02-16 14:18:20 +0800

    login 和 global.less

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,20 +1,36 @@
 import React from 'react';
 import { Button } from 'antd';
 import { router } from 'umi';
+import classNames from 'classnames';
+import LoginHeader from '@/components/LoginHeader';
+import styles from './index.less'
 
-const Login = () => {
-  return (
-    <div>
-      <h1>登录页面</h1>
-      <Button type="primary" onClick={() => router.push('/register')}>
-        去注册
-      </Button>
-      <br/>
-      <Button type="primary" onClick={() => router.push('/admin/welcome')}>
-        管理后台
-      </Button>
-    </div>
-  );
-};
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+  render() {
+    return (
+      <div className={classNames('cpau-login', styles.login)}>
+        <LoginHeader />
+        <div className={styles.loginContent}>
+          <div className={styles.formWarp}>
+            <h1>登录页面</h1>
+            <Button type="primary" onClick={() => router.push('/register')}>注册页面</Button>
+            <p></p>
+            <Button type="primary" onClick={() => router.push('/admin/welcome')}>管理员1登录</Button>
+            <p></p>
+            <Button type="primary" onClick={() => router.push('/admin/welcome')}>管理员2登录</Button>
+            <p></p>
+            <Button type="primary" className="cp-btn-primary">自定义按钮</Button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Login;
